f4f694676b75b0c8983f01bf51868cddd8661fb2
sakunisgithub <ananda2003biswas@gmial.com>
2022-12-13 21:36:08 +0530

    Initial Commit

---
+++ python_general/00041_match_case.py
@@ -0,0 +1,15 @@
+number = int(input("Enter a number between 1 & 5 : "))
+
+match number :
+    case 1 :
+        print("Yudhistir")
+    case 2 :
+        print("Arjun")
+    case 3 :
+        print("Bheem")
+    case 4 :
+        print("Nakul")
+    case 5 :
+        print("Sahadeb")
+    case _ :
+        print("Invalid input !!")
