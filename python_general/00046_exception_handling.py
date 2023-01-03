dbe02e92c305a319621ee9df72ee0648b755b37f
sakunisgithub <ananda2003biswas@gmial.com>
2023-01-04 01:33:50 +0530

    Initial Commit

--- python_general/00046_exception_handling.py
+++ python_general/00046_exception_handling.py
@@ -7,3 +7,10 @@ try :
 
 except Exception as e :
     print(e)
+
+finally :
+    print("I am always executed !")
+
+# the code within finally is always executed
+# the real significance of 'finally' is when we use try-except-finally chain within a user-defined function definition....
+# even if the function gets returned, the code in the finally block is always executed
